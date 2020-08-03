fb8f87255c3f1b2739a3cb6aa5d1685b2bef21df
nobodyInbody <nobodyInbody@outlook.com>
2020-08-03 16:30:33 +0900

    Create CreatCtrlVisibility.py

---
+++ CreatCtrlVisibility.py
@@ -0,0 +1,6 @@
+rt = pymxs.runtime
+obj = rt.selection[0]
+ref_ctrlPos_node = rt.CirCle()
+ctrl_node = rt.CirCle()
+ref_ctrlPos_node.name = 'RefPos_Ctrl_Visibility'
+ctrl_node.name = 'Ctrl_visibility'
