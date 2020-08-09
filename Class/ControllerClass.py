ffcf6f8c9460c1aa4cd42fb6b1db87353f658c74
살균표백 <NobodyInbody@outlook.com>
2020-08-10 01:44:36 +0900

    test

--- Class/ControllerClass.py
+++ Class/ControllerClass.py
@@ -1,4 +1,15 @@
 import pymxs
-
-class ControllerClass()
-    
+rt = pymxs.runtime
+class ControllerClass():
+    m_rot_name = 'Rotation'
+    m_pos_name = 'Position'
+    m_scale_name = 'Scale'
+    def __init__(self):
+        pass
+    def SetBoneLength(self, bone, length):
+        bone.length = length
+    def SetTcbRotController(self, objs):
+        value_name = ''
+        for obj in objs:
+            tcb_rot = rt.TCB_Rotation()
+            enable_controller = rt.getPropertyController(obj, self.m_rot_name)
